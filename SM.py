636e8458b489137d3ac38313ccdabfa4bec14108
Полина Тригуб PD-87 <doc21@inbox.ru>
2024-02-07 20:07:08 +0300

    task 2

--- SM.py
+++ SM.py
@@ -6,7 +6,17 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+
+    def rate_lec(self,lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = grade
+        else:
+            return 'Ошибка'
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -23,10 +33,17 @@ class Mentor:
             return 'Ошибка'
 
 class Lecturer(Mentor):
-    pass
+    grades = {}
 
 class Reviewer(Mentor):
-    pass
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 
